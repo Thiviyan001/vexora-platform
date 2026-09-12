c402159641cf42b821f92ae69533d85dc84329e2
Thiviyan001 <vimalgoretti@gmail.com>
2026-09-12 15:54:34 +0530

    feat: load Clearview AI chapter visual

--- app/case-study/[chapter]/layout.tsx
+++ app/case-study/[chapter]/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import "../../detail.css";
 import "../research-layer.css";
+import "../clearview.css";
 import CaseStudyEnhancements from "../CaseStudyEnhancements";
 
 export default function ChapterLayout({ children }: Readonly<{ children: ReactNode }>) {
