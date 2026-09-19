6e3fe02e72749a4c9391fcd396ea72d4cb4409de
Thiviyan001 <vimalgoretti@gmail.com>
2026-09-19 23:29:37 +0530

    Serve VEXORA media files in Django API

--- backend/vexora/urls.py
+++ backend/vexora/urls.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-urlpatterns=[path("admin/",admin.site.urls),path("api/",include("social.urls"))]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("social.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
